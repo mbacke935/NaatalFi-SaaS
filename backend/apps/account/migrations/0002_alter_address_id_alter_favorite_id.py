57d644b4f2217a9e389c475f87fe596c6f955c79
Mohamed Mbacke <mbackem500@gmail.com>
2026-06-11 20:27:26 +0000

    migrate phase 11

---
+++ backend/apps/account/migrations/0002_alter_address_id_alter_favorite_id.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='address',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+        migrations.AlterField(
+            model_name='favorite',
+            name='id',
+            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+        ),
+    ]
