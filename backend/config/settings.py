d637ddb2fcd8d51d985ec75f9b92da699c0b7d5b
Mohamed Mbacke <mbackem500@gmail.com>
2026-06-12 19:31:41 +0000

    Add email timeout for cron

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,6 +160,7 @@ else:
     EMAIL_USE_TLS    = True
     EMAIL_HOST_USER  = os.getenv('EMAIL_HOST_USER', '')
     EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+    EMAIL_TIMEOUT    = int(os.getenv('EMAIL_TIMEOUT', 10))
 
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'NaatalFi <noreply@naatalfi.com>')
 
